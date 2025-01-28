8928fd53bd92b692cb43667b66042ef7f4542036
kir <kyryloshy@gmail.com>
2025-01-28 18:29:01 +0100

    Changed the main kompiler.rb file to include kompiler/config.rb

--- lib/kompiler.rb
+++ lib/kompiler.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'kompiler/config.rb'
 require 'kompiler/mc_builder.rb'
 require 'kompiler/parsers.rb'
 require 'kompiler/compiler_functions.rb'
