79b0ac5768d47b3c53a3b4bfa08f4f37f10cb2b0
kir <kyryloshy@gmail.com>
2025-01-28 18:33:03 +0100

    Changed .load and .load_end to be directives instead of fixed pre-processor functions. Added the .value and .macro directives. In Kompiler::CompilerFunctions.parse_code(), gave more control to directives and more state configurations. Changed Kompiler::CompilerFunctions.compile to reflect the changes regarding includes and macros.

--- lib/kompiler/compiler_functions.rb
+++ lib/kompiler/compiler_functions.rb
@@ -5,222 +5,33 @@ module Kompiler
 
 module CompilerFunctions
 
-def self.parse_includes(lines, loaded_files=[])
-	
-	final_lines = lines.dup	
-	
-	line_i = 0
-	
-	loop do
-		break if line_i >= final_lines.size
-		
-		line = final_lines[line_i]
-		
-		keyword, operands = Kompiler::Parsers.parse_instruction_line(line)
-		
-		if keyword == false
-			line_i += 1
-			next
-		end
-		
-		if keyword.start_with? "."
-			keyword = keyword[1..]
-		end
-		
-		if !["load", "include", "load_end", "include_end"].include?(keyword)
-			line_i += 1
-			next
-		end
-		
-		raise "Incorrect use of the \"#{keyword}\" directive: requires a filename in a string." if operands.size != 1 || (operands.size == 1 && operands[0][:type] != "string")
-		
-		load_file_name_selector = operands[0][:string]
-
-
-		# Remove the include code line from the lines array
-		final_lines.delete_at line_i
-
-		# If ends with _end, that means that the file contents should be appended at the end of the current lines
-		if keyword.end_with? "_end"
-			next_i_insert = final_lines.size
-		else
-			# If doesn't end with _end, files should be loaded in-place of the current line
-			next_i_insert = line_i
-		end
-		
-		
-		Dir[load_file_name_selector].each do |load_file_name|
-			# Get the absolute path filename
-			full_file_name = File.expand_path(load_file_name)
-			
-			raise "#{keyword} \"#{load_file_name}\": File not found." if !File.exist?(full_file_name)
-			
-			# Check if the file was already loaded (stop recursive loading)
-			if loaded_files.include?(full_file_name)
-				next
-			end
-			
-			# Read the file to load
-			include_code = File.read(full_file_name)
-			
-			# Separate the lines inside it
-			include_code_lines = Kompiler::Parsers.get_code_lines(include_code)
-			
-			# Add the lines from the load file to the lines array at the line_i index, effectively replacing the load command with the content of the load file
-			final_lines.insert next_i_insert, *include_code_lines
-			
-			next_i_insert += include_code_lines.size
-			
-			# Add the filename (absolute path) to the list of included files
-			loaded_files << full_file_name
-		end
-		
-		# Don't increment line_i, since the new loop will now start include-parsing the newly loaded file in the same program
-	end
-
-	final_lines
-	
-end
-
-
-def self.parse_macros(lines)
-
-	macros = []
-
-	line_i = 0
-	
-	loop do
-		break if line_i >= lines.size
-		
-		line = lines[line_i]
-		
-		keyword, operands = Kompiler::Parsers.parse_instruction_line line
-		
-		keyword = keyword[1..] if keyword.start_with? "."
-		
-		if keyword == 'macro'
-			raise "Macro definition error: Expected two operands to be provided, while #{operands.size} were given" if operands.size != 2
-			raise "Macro definition error: The macro name must either be a word or a string" if !["label", "string"].include?(operands[0][:type])
-			
-			case operands[0][:type]
-			when "label"
-				macro_name = operands[0][:value]
-			when "string"
-				macro_name = operands[0][:value]
-			end
-			
-			macro_definition = operands[1][:definition]
-			
-			macros << {name: macro_name, definition: macro_definition}
-			
-			lines.delete_at line_i
-		else
-			line_i += 1
-		end
-	end
-	
-	# Apply the macros to all lines, and then the lines that were changed, and than again, and so on (iterative loop until none of the lines need to be checked)
-	
-	lines_to_check = (0...lines.size).to_a
-	
-	while lines_to_check.size != 0 # Repeat until no more lines to check		
-
-		# Define restricted separators characters (characters that CAN'T separate a macro from everything else)
-		restricted_separator_chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a + ["_", "."]
-		
-		# Create an array for the lines to check once again
-		new_lines_to_check = []
-		
-		lines_to_check.each do |line_i|
-			line = lines[line_i]
-			
-			char_i = 0
-			
-			# Create a variable to indicate if a macro was used on the current line 
-			line_macro_used = false
-			
-			loop do
-				break if char_i >= line.size
-				
-				if ['"', "'"].include? line[char_i]
-					# If a string, skip it
-					str, parsed_length = Kompiler::Parsers.parse_str line[char_i..]
-					char_i += parsed_length
-					next
-				end
-				
-				cut_line = line[char_i..]			
-				
-				# Create a variable to indicate if a macro was used in the current position
-				macro_used = false			
-				
-				macros.each do |macro|
-					macro_name = macro[:name]
-					macro_def = macro[:definition]
-					next if !cut_line.start_with?(macro_name) # Skip if doesn't begin with the macro's name
-					next if (cut_line.size > macro_name.size) && restricted_separator_chars.include?(cut_line[macro_name.size]) # Skip if there is a character after the macro name, and the character is not permitted
-					next if (char_i > 0) && restricted_separator_chars.include?(line[char_i - 1]) # Skip if there is a character before the checking sequence, and the character is not permitted
-					
-					# Here if the macro is 'accepted'
-					line = line[...char_i] + macro_def + line[(char_i + macro_name.size)..]
-					# Indicate that a macro was used
-					macro_used = true
-				end
-				
-				if macro_used
-					# If a macro was used, indicate that on line level
-					line_macro_used = true
-				else
-					# If a macro wasn't used, proceed to the next character
-					char_i += 1
-				end
-			end # line characters loop
-			
-			if line_macro_used
-				# If a macro was used on the current line, update the lines array, and add the line to check for macros again
-				lines[line_i] = line
-				new_lines_to_check << line_i
-			end
-		end # lines_to_check.each loop
-		
-		lines_to_check = new_lines_to_check # Update lines to check with the new ones
-	
-	end # while lines_to_check.size != 0 loop
-
-	lines
-end
-
-
 def self.parse_code(lines)	
 	
 	parsed_lines = []
 	
 	instr_adr = 0
 	
-	lines.each_with_index do |line, line_i|
+	line_i = 0
+
+	extra_state = Hash.new
+	
+	while line_i < lines.size
+		line = lines[line_i]
 		
 		# Check if line is not just whitespace
 		is_char_whitespace = line.each_char.map{|c| [" ", "\t"].include? c}
 		if !is_char_whitespace.include?(false) # If only whitespace
+			line_i += 1
 			next # Skip
 		end
 		
-		#
-		# Label definitions are now a directive, so this isn't needed
-		#
-		
-		# is_label, label_name = check_label(line)
-		# if is_label
-		# 	# labels[label_name] = instr_adr
-		# 	parsed_lines << {type: "label", label_name: label_name, label_address: instr_adr, address: instr_adr}
-		# 	next
-		# end
-	
 		is_instruction, exec_instruction = Kompiler::Parsers.check_instruction(line)
 		if is_instruction
 			parsed_lines << {type: "instruction", instruction: exec_instruction[:instruction], operands: exec_instruction[:operands], address: instr_adr}			
 			instr_adr += exec_instruction[:instruction][:bitsize] / 8
-			
+
+			line_i += 1
+		
 			next # Go to the next line
 		end
 		
@@ -230,12 +41,15 @@ def self.parse_code(lines)
 			directive = directive_hash[:directive]
 			operands = directive_hash[:operands]
 			
-			state = {current_address: instr_adr, parsed_lines: parsed_lines}
+			state = {current_address: instr_adr, parsed_lines: parsed_lines, lines: lines, line_i: line_i, extra_state: extra_state}
 			
 			state = directive[:func].call(operands, state)
 		
 			instr_adr = state[:current_address]
 			parsed_lines = state[:parsed_lines]
+			lines = state[:lines]
+			line_i = state[:line_i]
+			extra_state = state[:extra_state]
 			
 			next # Skip to the next lime
 		end
@@ -351,17 +165,9 @@ def self.compile(code, included_files=[])
 
 	lines = Kompiler::Parsers.get_code_lines(code)
 	
-	included_lines = parse_includes(lines, included_files.map{|fname| File.expand_path(fname)})
+	parsed_lines = parse_code(lines)
 	
-	macroed_lines = parse_macros(included_lines)
-	
-	parsed_lines = parse_code(macroed_lines)
-	
-	labels = get_labels(parsed_lines)	
-	
-#	machine_code_bit_lines = construct_program_mc(parsed_lines, labels)
-#
-#	machine_code_bytes = bit_lines_to_bytes(machine_code_bit_lines)	
+	labels = get_labels(parsed_lines)
 	
 	machine_code_bytes = construct_program_mc(parsed_lines, labels)
 end
--- lib/kompiler/directives.rb
+++ lib/kompiler/directives.rb
@@ -14,9 +14,12 @@ end
 		keyword: "zeros",
 		func: lambda do |operands, state|
 			raise "Incorrect use of the \"zeros\" directive." if operands.size > 1 || (operands[0] && operands[0][:type] != "immediate")
+			
 			n_zeros = operands[0][:value]
 			state[:current_address] += n_zeros
 			state[:parsed_lines] << {type: "insert", bits: (n_zeros*8).times.map{0} }
+			state[:line_i] += 1
+			
 			state
 		end
 	},
@@ -30,6 +33,7 @@ end
 			
 			state[:parsed_lines] << {type: "insert", bits: insert_bits, address: state[:current_address]}
 			state[:current_address] += insert_bytes.size
+			state[:line_i] += 1
 			
 			state
 		end
@@ -38,6 +42,8 @@ end
 		keyword: "align",
 		func: lambda do |operands, state|
 			raise "Incorrect use of the \"align\" directive." if operands.size > 1 || (operands[0] && operands[0][:type] != "immediate")
+			
+			state[:line_i] += 1
 			
 			alignment = operands[0][:value]
 			to_add = alignment - (state[:current_address] % alignment)			
@@ -72,6 +78,7 @@ end
 			
 			# Add the label definition
 			state[:parsed_lines] << {type: "label", label_name: label_name, label_address: label_value, address: state[:current_address]}
+			state[:line_i] += 1
 			
 			state
 		end
@@ -89,6 +96,7 @@ end
 			# Insert 64 bits of the value into the program
 			state[:current_address] += 8
 			state[:parsed_lines] << {type: "insert", bits: value_bits}
+			state[:line_i] += 1
 			
 			state
 		end
@@ -106,6 +114,7 @@ end
 			# Insert 64 bits of the value into the program
 			state[:current_address] += 4
 			state[:parsed_lines] << {type: "insert", bits: value_bits}
+			state[:line_i] += 1
 			
 			state
 		end
@@ -116,6 +125,7 @@ end
 			
 			raise "Incorrect use of the \"bytes\" directive." if (operands.size != 2) || (operands[0][:type] != "immediate" && operands[1][:type] != "immediate")			
 			
+			
 			n_bytes = operands[0][:value]
 			value = operands[1][:value]
 			
@@ -124,6 +134,7 @@ end
 			# Insert the input amount of bytes of the value into the program
 			state[:current_address] += n_bytes
 			state[:parsed_lines] << {type: "insert", bits: value_bits}
+			state[:line_i] += 1
 			
 			state
 		end
@@ -132,11 +143,12 @@ end
 		keyword: "set_pc",
 		func: lambda do |operands, state|
 			
-			raise "Incorrect use of the \"set_pc\" directive." if (operands.size != 1) || (operands[0][:type] != "immediate")			
+			raise "Incorrect use of the \"set_pc\" directive." if (operands.size != 1) || (operands[0][:type] != "immediate")						
 			
 			new_pc = operands[0][:value]
-
+			
 			state[:current_address] = new_pc
+			state[:line_i] += 1
 			
 			state
 		end
@@ -155,7 +167,340 @@ end
 
 			state[:current_address] += file_content.bytes.size
 			state[:parsed_lines] << {type: "insert", byte_string: file_content}
+			
+			state[:line_i] += 1
+			
+			state
+		end
+	},
+	{
+		keyword: ["load_end", "include_end"],
+		func: lambda do |operands, state|
 
+			raise "Incorrect use of the \"load_end\" directive" if (operands.size != 1) || (operands[0][:type] != "string")
+
+			file_selector = operands[0][:string]
+
+			files_to_load = Dir[file_selector]
+
+			# Create the loaded_files state entry if it doesn't exist
+			state[:extra_state][:loaded_files] = Array.new if !state[:extra_state].keys.include?(:loaded_files)
+
+			# Select only files that haven't been previously loaded
+			files_to_load.select!{|file_name| !state[:extra_state][:loaded_files].include?(file_name)}
+
+			# Add the files that will be loaded to the state entry
+			state[:extra_state][:loaded_files] += files_to_load
+
+			files_to_load.each do |load_filename|
+				file_content = File.read load_filename
+
+				file_lines = Kompiler::Parsers.get_code_lines(file_content)
+
+				state[:lines] += file_lines
+			end
+
+			state[:line_i] += 1
+
+			state
+		end
+	},
+	{
+		keyword: ["load", "include"],
+		func: lambda do |operands, state|
+
+			raise "Incorrect use of the \"load\" directive" if (operands.size != 1) || (operands[0][:type] != "string")
+
+			file_selector = operands[0][:string]
+
+			files_to_load = Dir[file_selector]
+
+			# Create the loaded_files state entry if it doesn't exist
+			state[:extra_state][:loaded_files] = Array.new if !state[:extra_state].keys.include?(:loaded_files)
+
+			# Select only files that haven't been previously loaded
+			files_to_load.select!{|file_name| !state[:extra_state][:loaded_files].include?(file_name)}
+
+			# Add the files that will be loaded to the state entry
+			state[:extra_state][:loaded_files] += files_to_load
+
+			total_load_lines = []
+
+			files_to_load.each do |load_filename|
+				file_content = File.read load_filename
+
+				file_lines = Kompiler::Parsers.get_code_lines(file_content)
+
+				total_load_lines += file_lines
+			end
+
+			# Move to the next line
+			state[:line_i] += 1
+
+			# Insert the lines at the correct place
+			state[:lines] = state[:lines][0...state[:line_i]] + total_load_lines + state[:lines][state[:line_i]..]
+
+			state
+		end
+	},
+	{
+		keyword: "value",
+		collect_operands: false,
+		func: lambda do |_, state|			
+			_, operands = Kompiler::Parsers.extract_instruction_parts(state[:lines][state[:line_i]])
+			
+			raise "Incorrect use of the .value directive - expected 2 operands: Program build not possible" if operands.size != 2
+			
+			value_name = operands[0]
+			
+			# Check that the name is made out of allowed characters
+			if value_name.each_char.map{|char| Kompiler::Config.keyword_chars.include?(char)}.include?(false)
+				raise "Incorrect use of the .value directive - the value name must contain only keyword characters: Program build not possible"
+			end
+			
+			value_def = operands[1]
+			
+			scan_lines = state[:lines][(state[:line_i] + 1)..]
+			
+			scan_lines.each_with_index do |line, line_i|
+				
+				start_i = 0
+				
+				# Loop through each character starting position
+				while start_i < line.size					
+					
+					# Skip whitespace characters
+					if Kompiler::Config.whitespace_chars.include?(line[start_i])
+						start_i += 1
+						next
+					end
+					
+					# Skip string definitions
+					if ['"', "'"].include? line[start_i]
+						str, parsed_length = Kompiler::Parsers.parse_str line[start_i..]
+						start_i += parsed_length
+						next
+					end
+					
+					cut_line = line[start_i..]
+					
+					value_word_found = false
+					
+					# Check if the value name works
+					
+					if cut_line.start_with?(value_name) # Check that the piece of text starts with the value name
+						if !Kompiler::Config.keyword_chars.include?(cut_line[value_name.size]) # Check that the cut text is a full word. This will not fire when value_name='arg', but the cut text is 'arg1'
+							value_word_found = true # Indicate that a replacement was found
+							
+							scan_lines[line_i] = scan_lines[line_i][...start_i] + value_def + (scan_lines[line_i][(start_i+value_name.size)..] || "")
+							line = scan_lines[line_i]
+						end
+					end
+					
+					# Check if the value name wasn't detected
+					# If not, skip the text until the next whitespace character
+					if !value_word_found
+						while start_i < line.size && Kompiler::Config.keyword_chars.include?(line[start_i])
+							start_i += 1
+						end
+						start_i += 1 # Move one more character
+					end
+					
+				end
+				
+			end
+			
+			state[:lines] = state[:lines][...state[:line_i]] + scan_lines
+			
+			state[:line_i] += 1
+			
+			state
+		end
+	},
+	{
+		keyword: "macro",
+		collect_operands: false,
+		func: lambda do |_, state|
+			line_i = state[:line_i]
+			
+			def_line = state[:lines][line_i]
+			
+			# First: collect the part after ".macro"
+			
+			char_i = 0
+			# Skip the whitespace before .macro
+			while char_i < def_line.size && Kompiler::Config.whitespace_chars.include?(def_line[char_i])
+				char_i += 1
+			end
+			# Skip the .macro
+			while char_i < def_line.size && Kompiler::Config.keyword_chars.include?(def_line[char_i])
+				char_i += 1
+			end
+			# Skip the whitespace after .macro
+			while char_i < def_line.size && Kompiler::Config.whitespace_chars.include?(def_line[char_i])
+				char_i += 1
+			end
+			
+			# If the end of the line was reached, throw an error
+			raise "Incorrect .macro definition" if char_i == def_line.size
+			
+			# Now char_i contains the first index of the text after .macro
+			
+			macro_def = def_line[char_i..]
+			
+			# Second: extract the macro's name
+			
+			macro_name = ""			
+			
+			while char_i < def_line.size && Kompiler::Config.keyword_chars.include?(def_line[char_i])
+				macro_name << def_line[char_i]
+				char_i += 1
+			end
+			
+			# Third: extract the operand names (code taken from parse_instruction_line in parsers.rb)
+			
+			arg_names = Kompiler::Parsers.extract_instruction_operands(def_line[char_i..])
+			
+			# Make sure that the arg names are unique
+			raise "Macro definition error - arguments cannot have the same name: Program build not possible" if arg_names.size != arg_names.uniq.size
+			
+			# Extract the macro inside definition
+			
+			line_i = state[:line_i] + 1
+			def_lines = []
+			
+			whitespace_regexp = /[#{Kompiler::Config.whitespace_chars.join("|")}]*/
+			
+			endmacro_regexp = /\A#{whitespace_regexp}\.endmacro#{whitespace_regexp}\z/
+			
+			while line_i < state[:lines].size
+				break if state[:lines][line_i].match? endmacro_regexp # Check if it's an end macro instruction
+				def_lines << state[:lines][line_i]
+				line_i += 1
+			end
+			
+			
+			# Find insert indexes for each argument
+			arg_insert_locations = arg_names.map{|arg_name| [arg_name, []]}.to_h
+			
+			def_lines.each_with_index do |line, line_i|
+				
+				start_i = 0
+				
+				# Loop through each character starting position
+				while start_i < line.size					
+					
+					# Skip whitespace characters
+					if Kompiler::Config.whitespace_chars.include?(line[start_i])
+						start_i += 1
+						next
+					end
+					
+					# Skip string definitions
+					if ['"', "'"].include? line[start_i]
+						str, parsed_length = Kompiler::Parsers.parse_str line[start_i..]
+						start_i += parsed_length
+						next
+					end
+					
+					cut_line = line[start_i..]
+					
+					arg_found = false
+					
+					# Check if one of the argument names works
+					arg_names.each do |arg_name|
+						next if !cut_line.start_with?(arg_name) # Skip the argument if the line doesn't begin with it
+						next if Kompiler::Config.keyword_chars.include?(cut_line[arg_name.size]) # Skip if the argument is a partial word. So, for the argument 'arg', this will skip in the case of 'arg1'
+						# Here if the argument name should be replaced with the contents
+						arg_found = true # Indicate that a replacement was found						
+						
+						arg_insert_locations[arg_name] << [line_i, start_i] # Add the insert location to the list
+						
+						# start_i += arg_name.size
+						def_lines[line_i] = def_lines[line_i][...start_i] + (def_lines[line_i][(start_i+arg_name.size)..] || "")
+						line = def_lines[line_i]
+						
+						break # Skip the arguments loop
+					end
+					
+					# Check if an argument was found
+					# If not, skip the text until the next whitespace character
+					if !arg_found
+						while start_i < line.size && Kompiler::Config.keyword_chars.include?(line[start_i])
+							start_i += 1
+						end
+						start_i += 1 # Move one more character
+					end
+					
+				end
+				
+			end
+			
+			state[:extra_state][:macros] = Array.new if !state[:extra_state].keys.include?(:macros)
+			
+			state[:extra_state][:macros] << {name: macro_name, args: arg_names, def_lines: def_lines, arg_insert_locations: arg_insert_locations}
+			
+			
+			# Scan the lines after the macro for the macro call and replace it with the macro definition
+			
+			scan_lines = state[:lines][(state[:line_i] + def_lines.size + 1 + 1)..]
+			
+			line_i = 0
+			
+			# Re-group argument insert locations by line -> [index, arg index]			
+			
+			arg_insert_locations_regrouped = def_lines.size.times.map{[]}
+			
+			arg_insert_locations.each do |arg_name, insert_locations|
+				insert_locations.each do |line_i, char_i|
+					arg_insert_locations_regrouped[line_i] << [char_i, arg_names.index(arg_name)]
+				end
+			end
+			
+			
+			while line_i < scan_lines.size
+				keyword, operands = Kompiler::Parsers.extract_instruction_parts(scan_lines[line_i])
+				
+				# If parsing failed, move on to the next line
+				if keyword == false
+					line_i += 1
+					next
+				end
+				
+				# If the keyword isn't the macro's name, skip the line
+				if keyword != macro_name
+					line_i += 1
+					next
+				end
+				
+				# Here when the keyword matches the macro name
+				
+				# Check that the number of operands is correct
+				if operands.size != arg_names.size
+					raise "Incorrect use of the \"#{macro_name}\" macro - #{arg_names.size} operands expected, but #{operands.size} were given: Program build not possible."
+				end
+				
+				# Build the replacement lines for the macro call
+				build_lines = def_lines.dup
+				
+				arg_insert_locations_regrouped.each_with_index do |locations, line_i|
+					# Sort the locations by the insert character from largest to smallest, so that the inserts are made from end to start
+					locations.sort_by{|el| el[0]}.reverse.each do |char_i, arg_index|
+						build_lines[line_i].insert char_i, operands[arg_index]
+					end
+				end
+				
+				# Replace the macro call with the built lines
+				scan_lines = scan_lines[...line_i] + build_lines + scan_lines[(line_i + 1)..]
+				
+				# Skip the inserted macro
+				line_i += build_lines.size
+			end			
+			
+			state[:lines] = state[:lines][...state[:line_i]] + scan_lines
+			
+			state[:line_i] += 1
+			
 			state
 		end
 	}
